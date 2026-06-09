2370d0d4f9ccbc616a9e0698df7cf6fd48009cf4
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2026-06-09 20:28:45 +0600

    base url update

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,10 +1,12 @@
 class AppConfig {
-  static const String baseUrl = "https://cadevapicdn02.freeli.io"; // API Host
+  // static const String baseUrl = "https://cadevapicdn02.freeli.io";
+  static const String baseUrl = "https://caapicdn02.freeli.io";
   static const String graphqlUrl = "$baseUrl/workfreeli";
 
   static const String fileServerUrl = "http://62.151.182.241:4055";
 
-  static const String xmppDomain = "caquecdn03.freeli.io";
+  // static const String xmppDomain = "caquecdn03.freeli.io";
+  static const String xmppDomain = "caquecdn02.freeli.io:5443";
   static const String xmppWsUrl = "wss://$xmppDomain:5443/ws";
 
   static Map<String, String> getUrls() {
